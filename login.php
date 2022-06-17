72c2450917eb9f12c443b9ec0f483532aa475fa2
aimedidierm <aimedidiermugisha@gmail.com>
2022-06-17 14:04:14 +0200

    Admin login auth

--- login.php
+++ login.php
@@ -12,7 +12,6 @@ if ($stmt->rowCount()>0) {
 	$_SESSION['id'] = session_id();
 	$_SESSION['login_type'] = "Seller";
 	echo "<script>window.location.assign('seller/dashboard.php')</script>";
-
 }
 $query = "SELECT id, email FROM school WHERE email= ? AND password = ? limit 1";
 $stmt = $db->prepare($query);
@@ -23,34 +22,17 @@ if ($stmt->rowCount()>0) {
 	$_SESSION['id'] = session_id();
 	$_SESSION['login_type'] = "School";
 	echo "<script>window.location.assign('school/dashboard.php')</script>";
-
-}/*
-$query = "SELECT * FROM manager WHERE username= ? AND password = ? AND status = '0' limit 1";
-$stmt = $db->prepare($query);
-$stmt->execute(array($code, $password));
-$rows = $stmt->fetchAll();
-if ($stmt->rowCount()>0) {
-	$_SESSION['code'] = $code;
-	$_SESSION['id'] = session_id();
-	$_SESSION['login_type'] = "Manager";
-
-	// echo "<script>alert('You Are Logged In');window.location.assign('manager/dashboard.php')</script>";
-	echo "<script>window.location.assign('manager/dashboard.php')</script>";
-
 }
-$query = "SELECT * FROM admin WHERE username= ? AND password = ? AND status = '0' limit 1";
+$query = "SELECT id, email FROM admin WHERE email= ? AND password = ? limit 1";
 $stmt = $db->prepare($query);
-$stmt->execute(array($code, $password));
-$rows = $stmt->fetchAll();
+$stmt->execute(array($email, $password));
+$rows = $stmt->fetch(PDO::FETCH_ASSOC);
 if ($stmt->rowCount()>0) {
-	$_SESSION['userid'] = $code;
+	$_SESSION['code'] = $email;
 	$_SESSION['id'] = session_id();
 	$_SESSION['login_type'] = "Admin";
-
-	// echo "<script>alert('You Are Logged In');window.location.assign('admin/dashboard.php')</script>";
 	echo "<script>window.location.assign('admin/dashboard.php')</script>";
-
-}*/else{
+}else{
 	echo "<script>alert('Your ID or Password is Wrong');window.location.assign('index.php')</script>";
 }
 ?>
