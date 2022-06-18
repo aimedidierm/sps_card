ab5fcbce0bc99c4662cb76801453978fe166b4a8
aimedidierm <aimedidiermugisha@gmail.com>
2022-06-18 18:59:02 +0200

    Nav bar

---
+++ seller/blank.php
@@ -0,0 +1,52 @@
+<?php
+require '../php-includes/connect.php';
+require 'php-includes/check-login.php';
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Seller</title>
+	<!-- Bootstrap Styles-->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FontAwesome Styles-->
+    <link href="../assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- Custom Styles-->
+    <link href="../assets/css/custom-styles.css" rel="stylesheet" />
+     <!-- Google Fonts-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <?php require 'php-includes/nav.php';?>
+        <div id="page-wrapper">
+		  <div class="header"> 
+                        <h1 class="page-header">Blank</h1>
+						<ol class="breadcrumb">
+					  <li><a href="dashboard.php">Home</a></li>
+					  <li class="active">Blank</li>
+					</ol> 
+									
+		</div>
+            <div id="page-inner"> 
+				 <footer><p>All right reserved.</p></footer>
+				</div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="../assets/js/jquery-1.10.2.js"></script>
+      <!-- Bootstrap Js -->
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="../assets/js/jquery.metisMenu.js"></script>
+      <!-- Custom Js -->
+    <script src="../assets/js/custom-scripts.js"></script>
+    
+   
+</body>
+</html>
